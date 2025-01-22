e5bb352a7e093bdf5d9c75b120c7a570f6ea9b7a
Evseev Roman <evseeff.roman@mail.ru>
2025-01-22 22:36:49 +0300

    add fixture

--- New_Project/test_example.py
+++ New_Project/test_example.py
@@ -2,12 +2,18 @@ from selenium.webdriver import Chrome
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+import pytest
+
+@pytest.fixture()
+def set_up_browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Включаем headless режим
+    driver = Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    yield driver
+    driver.quit()
 
 class TestExample:
-   def test_example(self):
-       chrome_options = Options()
-       chrome_options.add_argument("--headless")  # Включаем headless режим
-       driver = Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+   def test_example(self, set_up_browser):
+       driver = set_up_browser
        driver.get("http://skillbox.ru")
        assert "Skillbox – образовательная платформа с онлайн-курсами." == driver.title
-       driver.quit()
