6e67a43d33cb6ddc7078a8da326da7ff940c12c0
Evseev Roman <evseeff.roman@mail.ru>
2025-02-02 23:28:04 +0300

    add files: init, tox, conftest, Makefile, browser

---
+++ New_Project/src/browser.py
@@ -0,0 +1,13 @@
+from selenium.webdriver import Chrome
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+@pytest.fixture()
+def set_up_browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Включаем headless режим
+    driver = Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    yield driver
+    driver.quit()
--- New_Project/test_example.py
+++ New_Project/test_example.py
@@ -1,19 +1,20 @@
-from selenium.webdriver import Chrome
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
-import pytest
-
-@pytest.fixture()
-def set_up_browser():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Включаем headless режим
-    driver = Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-    yield driver
-    driver.quit()
-
 class TestExample:
-   def test_example(self, set_up_browser):
+   def test_example_1(self, set_up_browser):
        driver = set_up_browser
        driver.get("http://skillbox.ru")
        assert "Skillbox – образовательная платформа с онлайн-курсами." == driver.title
+
+   def test_example_2(self, set_up_browser):
+       driver = set_up_browser
+       driver.get("http://skillbox.ru")
+       assert "Skillbox – образовательная платформа  онлайн-курсами." == driver.title
+
+   def test_example_3(self, set_up_browser):
+       driver = set_up_browser
+       driver.get("http://skillbox.ru")
+       assert "Skillbox – образовательная платформа  онлайн-курсами." == driver.title
+
+   def test_example_4(self, set_up_browser):
+       driver = set_up_browser
+       driver.get("http://skillbox.ru")
+       assert "Skillbox – образовательная платформа  онлайн-курсами." == driver.title
