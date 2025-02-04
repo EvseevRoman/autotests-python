5c80aed4621f2396a2d81306ddf41c51f60d93b1
Evseev Roman <evseeff.roman@mail.ru>
2025-02-04 23:44:20 +0300

    фÐall bad

--- New_Project/src/tests/test_first.py
+++ New_Project/src/tests/test_first.py
@@ -2,7 +2,7 @@ class TestExample:
    def test_example_1(self, set_up_browser):
        driver = set_up_browser
        driver.get("http://skillbox.ru")
-       assert "Skillbox – образовательная платформа с онлайн-курсами." == driver.title
+       assert "Skillbox – образовательная платформа  онлайн-курсами." == driver.title
 
    def test_example_2(self, set_up_browser):
        driver = set_up_browser
